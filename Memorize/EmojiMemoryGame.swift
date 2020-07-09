cba543c78dafa8963ded003fd11d254e563ac8c7
Manoli <igielskv@icloud.com>
2020-07-09 09:19:57 +0200

    Implements MVVM with MemoryGame struct and EmojiMemoryGame class and connects game model with ContentView.

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,32 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Manoli on 09/07/2020.
+//  Copyright © 2020 macForce.one. All rights reserved.
+//
+
+import Foundation
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    // MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
