dd4b562e70463bda45071989e0d5781033dd0c23
Manoli <igielskv@icloud.com>
2020-07-17 22:53:44 +0200

    Adds rotationEffect animation to cards if they're matched.

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -41,6 +41,8 @@ struct CardView: View {
                     .opacity(0.4)
                 Text(card.content)
                     .font(Font.system(size: fontSize(for: size)))
+                    .rotationEffect(Angle.degrees((card.isMatched ? 360 : 0)))
+                    .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : .default)
             }
             .cardify(isFaceUp: card.isFaceUp)
         }
